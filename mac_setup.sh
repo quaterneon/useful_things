3cdc5e36c73af43a48a85129ba3d41c58c6b1c68
Agata <agata@planet.com>
2018-10-18 16:32:45 -0700

    Mac + docker setup notes

--- mac_setup.sh
+++ mac_setup.sh
@@ -13,3 +13,9 @@ source ~/.bashrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 
+# docker setup
+# Install VirtualBox first
+docker-machine create --driver virtualbox default
+docker-machine env default
+eval $(docker-machine env default)
+docker ps  # to check that it worked
