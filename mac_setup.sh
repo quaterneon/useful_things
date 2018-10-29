93cebc1634df801f90933606184ebe1e0f834175
Agata Kargol <agata@C02W51MBHV2R.local>
2018-10-29 12:35:07 -0700

    mac setup update after rerunning again

--- mac_setup.sh
+++ mac_setup.sh
@@ -14,8 +14,15 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 
 # docker setup
+# Option 1 (Preferred)
+# Install Docker for Mac
+https://docs.docker.com/v17.12/docker-for-mac/install/#download-docker-for-mac
+
+# Option 2 
 # Install VirtualBox first
 docker-machine create --driver virtualbox default
 docker-machine env default
 eval $(docker-machine env default)
 docker ps  # to check that it worked
+
+# Better comparison of the docker options: https://docs.docker.com/v17.12/docker-for-mac/docker-toolbox/
